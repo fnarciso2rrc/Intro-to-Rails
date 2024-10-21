1ff9be16fc559ca155d6a9226041766590920f5e
Lianne Gabriel <liannegabriel519@yahoo.com>
2024-10-21 13:35:42 -0500

    Added validations to Customer model.

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,7 @@
 class Customer < ApplicationRecord
   belongs_to :product
   belongs_to :dog
+
+  validates :first_name, :last_name, :age, presence: true
+  validates :age, numericality: { only_integer: true, :greater_than => 0 }
 end
