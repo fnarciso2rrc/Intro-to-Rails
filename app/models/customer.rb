756fb4b3524d25ee19da3f67a57d514eeb5fee39
Lianne Gabriel <liannegabriel519@yahoo.com>
2024-10-21 21:36:58 -0500

    Added validations to Product model & minor syntax change in Customer model.

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,5 +3,5 @@ class Customer < ApplicationRecord
   belongs_to :dog
 
   validates :first_name, :last_name, :age, presence: true
-  validates :age, numericality: { only_integer: true, :greater_than => 0 }
+  validates :age, numericality: { only_integer: true, :greater_than: 0 }
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,4 @@
 class Product < ApplicationRecord
+  validates :product_name, :category, :price, :description, presence: true
+  validates :price, numericality: {greater_than: 0}
 end
