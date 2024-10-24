d747bfc8bfbf69149f9390a9d09f9f0757299b7d
Frans Narciso <fransnarc@gmail.com>
2024-10-24 18:09:30 -0500

    Added many-to-many relationships to Customer and Product model. Remove : in Customer.rb

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,7 +1,8 @@
 class Customer < ApplicationRecord
   has_many :dogs
   has_many :customer_products
+  has_many :products, through: :customer_products
 
   validates :first_name, :last_name, :age, presence: true
-  validates :age, numericality: { only_integer: true, :greater_than: 0 }
+  validates :age, numericality: { only_integer: true, greater_than: 0 }
 end
