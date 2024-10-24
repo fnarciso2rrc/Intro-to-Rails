52f83ca6da5138bc7be89eb80a1be73bd02a814d
Lianne Gabriel <liannegabriel519@yahoo.com>
2024-10-24 09:40:49 -0500

    Modified Product model association

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord
+  has_many :customer_products
+
   validates :product_name, :category, :price, :description, presence: true
   validates :price, numericality: {greater_than: 0}
 end
