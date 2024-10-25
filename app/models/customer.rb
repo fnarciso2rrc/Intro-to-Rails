b7b34f1c7f51cafc0ebc3817ce9ca54e3b7ad401
Lianne Gabriel <liannegabriel519@yahoo.com>
2024-10-24 22:09:53 -0500

    Modified Customer table association

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,6 +1,7 @@
 class Customer < ApplicationRecord
   has_many :dogs
   has_many :customer_products
+  has_many :products, through: :customer_products
 
   validates :first_name, :last_name, :age, presence: true
   validates :age, numericality: { only_integer: true, :greater_than: 0 }
