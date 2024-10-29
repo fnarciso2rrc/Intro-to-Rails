2b9b011b75154f3cb3ba24e51f39955537dbba73
Lianne Gabriel <liannegabriel519@yahoo.com>
2024-10-29 10:59:08 -0500

    Fixed the SwitchDogIdToCustomerId migration to remove dog_id and add customer_id

--- db/migrate/20241029153800_switch_dog_id_to_customer_id.rb
+++ db/migrate/20241029153800_switch_dog_id_to_customer_id.rb
@@ -1,6 +1,7 @@
 class SwitchDogIdToCustomerId < ActiveRecord::Migration[7.2]
   def change
     remove_column :customers, :dog_id, :integer
-    add_column :dogs, :customer_id, :integer, foreign_key: true
+    add_column :dogs, :customer_id, :integer
+    add_foreign_key :dogs, :customers
   end
 end
