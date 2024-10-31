8f97760725e8cb9416e1ed0a0a31fb2c91fda620
Lianne Gabriel <liannegabriel519@yahoo.com>
2024-10-31 01:51:58 -0500

    Modified the pagination helper method to be implemented in the views

--- app/helpers/pagination_helper.rb
+++ app/helpers/pagination_helper.rb
@@ -3,6 +3,17 @@ module PaginationHelper
     offset = (page - 1) * per_page
     collection.limit(per_page).offset(offset)
   end
+
+  def page_link(collection, page, per_page)
+    links = ""
+
+    if page.to_i > 1
+      links = link_to "Previous", root_path(page: params[:page].to_i - 1)
+    end
+    if total_pages.size == per_page
+      link = link_to "Next", root_path(page: params[:page].to_i + 1)
+    end
+  end
 end
 
 # Source: https://www.geeksforgeeks.org/how-to-implement-pagination-in-ruby-on-rails/
