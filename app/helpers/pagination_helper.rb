e32a417f29847c4c35d03b2c2927ca5da87360e6
Lianne Gabriel <liannegabriel519@yahoo.com>
2024-10-31 17:46:23 -0500

    Fixed the pagination logic, implemented it in the customer, and added lang in the HTML

--- app/helpers/pagination_helper.rb
+++ app/helpers/pagination_helper.rb
@@ -1,20 +1,20 @@
 module PaginationHelper
-  def paginate(collection, page, per_page = 25)
+  def paginate(collection, page, per_page = 20)
     offset = (page - 1) * per_page
     collection.limit(per_page).offset(offset)
   end
 
-  def next_page(collection, chosen_path, per_size = 25)
+  def next_page(collection, chosen_path, per_size = 20)
     links = ""
     page = params[:page].to_i
-    next_link = page + 1
+    next_link = page == 0 ? 2 : page + 1
 
     if collection.size == per_size
       links = link_to "Next", send(chosen_path, page: next_link)
     end
   end
 
-  def previous_page(collection, chosen_path, per_size = 25)
+  def previous_page(collection, chosen_path, per_size = 20)
     links = ""
     page = params[:page].to_i
     previous_link = page - 1
