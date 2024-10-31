fddfdbce303f8e32222df0facf3f62e407aacde0
Lianne Gabriel <liannegabriel519@yahoo.com>
2024-10-31 16:19:31 -0500

    Fixed the pagination helper and implemented it on the customer

--- app/helpers/pagination_helper.rb
+++ app/helpers/pagination_helper.rb
@@ -4,17 +4,23 @@ module PaginationHelper
     collection.limit(per_page).offset(offset)
   end
 
-  def page_link(collection, chosen_path, per_size = 25)
+  def next_page(collection, chosen_path, per_size = 25)
     links = ""
     page = params[:page].to_i
-    previous_link = params[:page].to_i - 1
-    next_link = params[:page].to_i + 1
+    next_link = page + 1
+
+    if collection.size == per_size
+      links = link_to "Next", send(chosen_path, page: next_link)
+    end
+  end
+
+  def previous_page(collection, chosen_path, per_size = 25)
+    links = ""
+    page = params[:page].to_i
+    previous_link = page - 1
 
     if page > 1
       links = link_to "Previous", send(chosen_path, page: previous_link)
-    end
-    if collection.size == per_size
-      links = link_to "Next", send(chosen_path, page: next_link)
     end
   end
 end
