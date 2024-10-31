9cc1d7a89dfc23a8f4fe809b6fd98c500354e5af
Frans Narciso <fransnarc@gmail.com>
2024-10-31 12:35:09 -0500

    Added search method on Dog  model. Modified index method in dog controller, and added dog_params. Added a search form on Dog index page.

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,6 +1,11 @@
 class DogsController < ApplicationController
   def index
-    @dogs = Dog.all
+    @dogs = Dog.search(params[:search])
+  end
+  
+  private
+  def dog_params
+    params.require(:dog).permit(:dog_name, :id, :search)
   end
 
   def show
