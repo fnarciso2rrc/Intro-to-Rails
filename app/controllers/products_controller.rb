f6d7b4793280db727286a420a586e5cc20f87cf4
Lianne Gabriel <liannegabriel519@yahoo.com>
2024-10-31 19:18:56 -0500

    Implemented the pagination to product controller and view

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,14 +1,14 @@
 class ProductsController < ApplicationController
   def index
-    @products = Product.search(params[:search])
+    @products = Product.search(params[:search]).page(params[:page]).per(20)
   end
-  
+
   def show
     @product = Product.find(params[:id])
   end
 
   private
-  
+
   def product_params
     params.require(:product).permit(:product_name, :id, :search)
   end
